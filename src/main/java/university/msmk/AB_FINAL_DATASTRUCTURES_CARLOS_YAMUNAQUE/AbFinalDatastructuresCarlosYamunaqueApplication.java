704ae6c396c5018a57442a9465683055ed2f80d9
carlospygMSMK <carlospyg@msmk.university>
2025-11-04 20:06:09 +0100

    Mensaje commit, cambio completo de archivo

---
+++ src/main/java/university/msmk/AB_FINAL_DATASTRUCTURES_CARLOS_YAMUNAQUE/AbFinalDatastructuresCarlosYamunaqueApplication.java
@@ -0,0 +1,13 @@
+package university.msmk.AB_FINAL_DATASTRUCTURES_CARLOS_YAMUNAQUE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbFinalDatastructuresCarlosYamunaqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbFinalDatastructuresCarlosYamunaqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/university/msmk/AB_FINAL_DATASTRUCTURES_CARLOS_YAMUNAQUE/AbFinalDatastructuresCarlosYamunaqueApplicationTests.java
@@ -0,0 +1,13 @@
+package university.msmk.AB_FINAL_DATASTRUCTURES_CARLOS_YAMUNAQUE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbFinalDatastructuresCarlosYamunaqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
