33bc51c36e3a4402480d8d7fc88fd5d41bd57101
carlospygMSMK <carlospyg@msmk.university>
2025-11-04 20:46:52 +0100

    Actualización 1 del AB

--- src/main/java/university/msmk/AB_FINAL_DATASTRUCTURES_CARLOS_YAMUNAQUE/AbFinalDatastructuresCarlosYamunaqueApplication.java
+++ src/main/java/university/msmk/AB_FINAL_DATASTRUCTURES_CARLOS_YAMUNAQUE/AbFinalDatastructuresCarlosYamunaqueApplication.java
@@ -1,13 +1,32 @@
 package university.msmk.AB_FINAL_DATASTRUCTURES_CARLOS_YAMUNAQUE;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AbFinalDatastructuresCarlosYamunaqueApplication {
+import java.util.HashMap;
 
-	public static void main(String[] args) {
+@SpringBootApplication
+@Slf4j
+public class AbFinalDatastructuresCarlosYamunaqueApplication implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(AbFinalDatastructuresCarlosYamunaqueApplication.class);
+
+    public static void main(String[] args) {
 		SpringApplication.run(AbFinalDatastructuresCarlosYamunaqueApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args){
+
+        log.info("Hola");
+        HashMap<Integer, String > jugadoresDeDender = new HashMap<>();
+        jugadoresDeDender.put(15,"Jokic");
+        jugadoresDeDender.put(11,"Jokic");
+        String NombreJugadorEncontrado = jugadoresDeDender.get(11);
+        log.info("El jugador numero 11 es {}", NombreJugadorEncontrado);
+    }
 }
---
+++ src/main/java/university/msmk/AB_FINAL_DATASTRUCTURES_CARLOS_YAMUNAQUE/JugadorNBA.java
@@ -0,0 +1,18 @@
+package university.msmk.AB_FINAL_DATASTRUCTURES_CARLOS_YAMUNAQUE;
+
+public class JugadorNBA {
+    String Nombre;
+    int Puntos;
+    int Asistencias;
+    int Rebotes;
+
+    public JugadorNBA(String nombre, int puntos, int asistencias){
+        this.Nombre = nombre;
+        this.Puntos = puntos;
+        this.Asistencias = asistencias;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+}
